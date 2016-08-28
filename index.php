b96517aa62de5e89ee8270a2da15bfef75647cd5
naveen <kmnaveen101@gmail.com>
2016-08-28 17:43:52 +0530

    Add files via upload
    #new way  v.0.0.3 thorium
    new way is a php file manager, which has ability to upload your files to website without using your hosting file manager.
    i have added a new feature to thorium, to upload files to directories
    new way is protected by login
    the default values are 
    username: admin
    password: admin
    you can change according to your wish.

--- index.php
+++ index.php
@@ -1,92 +1,158 @@
-   
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="/path/to/jquery.mCustomScrollbar.concat.min.js"></script>
+
+
 <?php 
-
+ini_set('display_errors', 1);
 session_start();
+/*  newway v0.0.3 THORIUM | developed by naveen kingmaker | open source project | licensed under: open GPL(general public usage) license
+*/
+
+#######################################login details######################3333333
+##############################must change after downloading thi script######
+
+$username = "admin"; //change to your desired wish
+
+$password = "admin"; //change to your wish
+
+#these login credentials must be entered while you are acessing your website
 
 
-
-//deleting file
-class deletefile {
-
-	public function __construct($dest) {
-		$this->delete($dest);
-
-	}
-	public function delete ($dest) {
-
-
-unlink("$dest");
-header("location: index.php");
-		
-	}
-
-
-
-}
+###############################################################################3
 
 
 
 
 
 
-/* class for listing files*/
+
+
+
 
 class file {
-	public function __construct($dir) {
-$this->file($dir);
+
+
+public function __construct() {
+
+
+
+}
+public function upload($name, $tmp_name, $location) {
+
+
+
+
+
+
+move_uploaded_file($tmp_name, $location.$name);
+
+header('location: $location');
+
+
+
+}
+
+public function deletefile($arg1) {
+
+
+unlink($arg1);
+
+
+
+
+
+
+}
+public function viewfile($arg1) {
+
+
+if ($dh = opendir($arg1)) {
+
+while ($file = readdir($dh)) {
+
+$slash = "/";
+
+if (is_dir($arg1.$slash.$file)) {
+
+
+//directory
+echo "<div>
+<form action='index.php' method='POST'>
+<input type='hidden' value='$arg1$file$slash' name='dir'>
+<h4><i class='fa fa-folder'></i>
+&nbsp;&nbsp;<input type='submit'  value='$file' id='folder'>
+<div class='text-right'>
+<input type='hidden' value='$arg1$file' name='del'>
+<button type='submit'>
+<i class='fa fa-trash-o'></i></button>
+
+</div>
+</form>
+</div>"; 
+
+echo "</h4><hr/><br/>";
+
+}
+
+else {
+//file
+echo "
+<h4>
+<i class='fa fa-file'>
+</i>&nbsp;&nbsp;<a href='$arg1$file'>
+$file</a>
+<form action='index.php' method='POST'>
+<div class='text-right'>
+<input type='hidden' value='$arg1$file' name='del'>
+<button type='submit' class='l'>
+<i class='fa fa-trash-o'>
+</i>
+</button>
+<a class='l' href='$arg1$file' download>
+<i class='fa fa-download'></i>
+</a>
+</div>
+</form>
+</h4>"; 
+
+
+
+echo "</h1><hr/><br/>";
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+}
+public function deldir($arg1) {
+
+if ($dh = opendir($arg1)) {
+
+	while ($file = readdir($dh)) { 
+
+unlink($file);
+
 
 	}
 
-public function file($dir) {
-
-if ($dh = opendir($dir)) {
-
-while ($file = readdir($dh)) {
-	 $ext = pathinfo($file, PATHINFO_EXTENSION);
-
-
-	 if (empty($ext)) {
-
-echo "<form action='index.php' method='POST'><i class='fa fa-folder'></i>&nbsp;&nbsp;&nbsp;&nbsp;";
-
-echo "<input type='hidden' value='$dir$file/' name='dir'><input type='submit' value='$file' class='btn btn-link'></form>";
-
-
-
-
-	 }
-
-else {
-
-
-echo "<i class='fa fa-file-o'></i>&nbsp;&nbsp;&nbsp;&nbsp;";
-
-echo "<a href='$dir/$file' download>";
-echo $file, '</a>';
-
-
-echo "<div id='proxy'><form method='POST' action='index.php'><input type='hidden' name='file' value='$dir$file'><i class='fa fa-remove'></i><input type='submit' value='delete' class='submit btn btn-danger'>&nbsp;&nbsp;&nbsp;&nbsp;<a href='$dir$file' class='btn btn-success'><i class='fa fa-eye'></i>&nbsp;view</a></form></div><br/><br/>";
-
-}
-
-
-
-//if else loop ends before
-}
-
-
 
 
 }
-
-
-
-
-}
-
-
 
 }
 
@@ -94,20 +160,85 @@ echo "<div id='proxy'><form method='POST' action='index.php'><input type='hidden
 
 
 
-
-
-
-
-
-
-
-
+}
 
 
 
 ?>
 
 
+<?php 
+
+
+if (empty($_SESSION['uq'])) {
+
+
+
+
+echo '
+
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/font.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<title>Newway | the best filemanager </title>
+
+<div class="col-sm-4 co text-center">
+<i class="fa fa-shield" alt="my hobby project"></i>
+<h1> welcome to <b>Newway</b></h1>
+
+
+<div class="form-group">
+<form action="index.php" method="POST">
+<br/>
+<label>
+
+<input type="text" name="username" placeholder="username" class="form-control fftc" style="width: 290px;" maxlength="18">
+<br/>
+<input type="password" name="password" placeholder="password" class="form-control" style="width: 290px;">
+
+<div id="feedback"></div>
+
+
+<button type="submit" value = "" class="submitbutton">
+<i class="fa fa-sign-in"></i></button>
+</label>
+</form>
+</div>
+<br/>
+<br/>
+<br/>
+<br/>
+</div>
+
+
+
+
+<div class="col-sm-8 addbar text-center">
+
+<h1>Newway -a file explorer</h1>
+
+<div class="l1">
+<br/>
+
+easy way to explore your sites, with this new file manager
+
+</div>
+
+
+
+
+
+
+<div class="text-left">
+<div class="l2">
+<br/>
+<br/>
+<h1>
+<i class="fa fa-check"></i>&nbsp;
+a simple one file, file manager which almost does everything
+</u1>
 
 
 
@@ -115,265 +246,383 @@ echo "<div id='proxy'><form method='POST' action='index.php'><input type='hidden
 
 
 
+<div class="l3">
+<br/>
 
-<?php
+<h1>
+<i class="fa fa-check"></i>&nbsp;
+light weight, scalable, faster
 
-
-
-
-if (empty($_SESSION['name'])) {
-
-	/*
-
-
-display login page ...
+</h1>
 
 
 
+</div>
 
 
 
+<div class="l4">
+<br/>
+
+<h1>
+<i class="fa fa-check"></i>&nbsp;
+open source project, made better by many developers
+</h1>
 
 
 
+</div>
 
-	*/
 
-	echo '<!--header-->
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
-<!--end-->
+<div class="l5">
+<br/>
+
+<h1>
+<i class="fa fa-check"></i>&nbsp;
+secure file managing system , made by php
+</h1>
+
+
+
+</div>
+
+<div class="text-center">
+
+<div class="l5">
+<br/>
+<br/>
+<br/>
+<br/>
+
+
 <style>
-	.fa-shekel {
-		font-size: 90px;
+	.fa-github {
+		font-size: 40px;
 	}
-	body {
-		background-color: #eee;
-	}
-	.submit {
-		background-color: black;
-		opacity: 0.8;
-		padding:10px;
+	a:hover {
 		color: white;
-		border: 4px solid #eee;
-		border-radius: 60px;
 
 	}
-	.submit:active {
+</style>
+<h1><i class="fa fa-github"></i><br/>
+<br/>
+you can contribute to new way <a href="http://github.com/naveen17797/newway">here</a>
 
-	background-color: black;
-		opacity: 0.8;
-		padding:10px;
-		color: white;
-		border: 4px solid #eee;
-		border-radius: 60px;
+
+<br/>
+<br/>
+contributions help to grow us better</h1>
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+</h1>
+</div>
+</div>
+</div>
+
+<style type="text/css">
+.folder {
+	border: none;
+	background-color: transparent;
+}
+a {
+	color: white;
+}
+li {
+	font-family: fantasy;
+}
+.l1, .l2, .l3, .l4, .l5, .l6, .l7, .l8{
+	display: none;
+	font-family: sans-serif;
+
 
 
 
 	}
 	h1 {
-		font-family: monospace;
-		font-size: 30px;
+		font-size: 20px;
 	}
-	a {
-
-	}
+	.addbar {
 
 
-</style>
-<div class="col-sm-4">
+height: 800px;
+background-color: #d26456;
+color: white;
+font-family: monospace;
+display: none;
+
+
+
+}
+.tt {
+	background-color: transparent;
+}
+.yes {
+	background-color: transparent;	
+	height: 60px;
+	border: 2px solid #fff;
+	width: 300px;
+}
+.no {
+	background-color: transparent;	
+	height: 60px;
+	border: 2px solid #fff;
+	width: 300px;
+}
+.fa-shield {
+	font-size: 400px;
+	color: #d26456;
+
+
+}
+
+
+
+</style>';
+
+
+}
+
+
+
+
+elseif ($_SESSION['uq'] == "kwefheikhfiehfieuhtgirhgiuhriguhdighiuhgueihguiehgieuhgeiuhfgieuhfuiehtfiuehgiuehdguihdiughiuthgtiuhtur843t9854t894t9854yt8g7589yt756754t89g5498tg89tg489eyt4e9ytu498tu489eut894eut5894eut94uet98u4t89u4e9t8u54et8u48tu98tu4ut98u4ut9t54ue9tu9ut9t84") {
+
+
+
+
+
+
+
+
+#######################################after login #####################################################3
+
+if (!empty($_POST['dir'])) {
+
+
+$x = $_POST['dir'];
+
+$_SESSION['loc'] = $x;
+
+
+}
+
+else {
+
+$x  = "/var/www/html/";
+
+}
+if (empty($_POST['dir']) && !empty($_POST['del'])) {
+
+	$del = $_POST['del'];
+	//executes a  delete action on incoming directory
+
+	$det = new file;
+	$det->deletefile($del);
+
+}
+
+
+
+
+
+
+
+echo '
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/font.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<title>Newway | the best filemanager </title>
+
+';
+echo '<div class="jumbotron"><h3><i class="fa fa-shield"></i>&nbsp;new way <i class="fa fa-user" id="admin"></i>&nbsp;admin</h3></div>';
+
+echo '
+
+<div class="col-sm-12  color1 text-center">
+
+<h4>current directory: ', $x, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/><br/></h4><form action="index.php" method="POST" enctype="multipart/form-data"><input type="file" name="file" class="sudo"><br/><br/>
+<input type="submit"  value="upload" class="upload"></form>
+
+
+
+
+
+<div class="col-sm-10 color2 text-left">
+
+
+';
+
+$file = new file;
+$file->viewfile($x);
+
+echo '
 </div>
-<div class="col-sm-4 text-center bor">
-<br/><br/><br/><br/><br/><br/><br/>
-<i class="fa fa-shekel"></i>
-<h1>file manager</h1>
-<br/>
-<form method="POST" action="index.php">
-<input type="text" class="form-control" placeholder="username" name="username"><br/>
-<input type="password" class="form-control" placeholder="password" name="password">
-<br/>
-<input type="submit" class="submit btn btn-default" value="login">
-</form>
-<br/><br/>
-<a href="#">forget password?</a>
 </div>
-<div class="col-sm-4">
-</div>
+
+
+
+
+
+
+
+
 ';
 
 
-}
-else {
-if (!empty($_POST['dir'])) {
-	$dir = $_POST['dir'];
 
-}
-else {
-$dir = "../";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "<style>
+.sudo {
+
+	margin-left: 40%;
 }
 
-	/*display the list of folders and files in the diectory */
+.upload {
 
-	echo '<!--header-->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 
-<!--end-->
-<style>
-
-.fa-folder {
-	color: #3399ff;
+	color: black;
 }
-.box {
 
-	height: 790px;
-	
-	overflow-y: scroll;
-	margin-bottom: 10px;
-		background-color: black;
-		font-size: 40px;
-	
-
-}
-.col-sm-3 {
-height: 790px;
-background-color: black;
-color: #3399ff;
-	
-
-}
-hr {
-  border-top: 1px solid #fff;
-}
-.fa-shekel {
-font-size: 90px;
-}
-#plate {
-padding: 0px;
-	background-color: #339944;
+.color1 {
+	background-color: #d26456;
+	height: 8x00px;
 	display: none;
-color: white;
-}
-a{
 	color: white;
+	font-family: fantasy;
 }
+.color2 {
+		background-color: #fff;
+	height: 550px;
+	margin-top: 3em;
+
+	margin-bottom: 7em;
+		margin-right: 7em;
+
+	margin-left: 7em;
+	display: none;
+	overflow-y: scroll;
+	color: black;
+	
+}
+#admin {
+	
+	margin-left: 43em;
+}
+
+.fa-trash-o {
+	color: red;
+}
+
+
+.jumbotron {
+
+	background-color: #d26456;
+	color: white;
+	padding: 5px;
+	margin-bottom: 0px;
+}
+.fa-shield {
+	color: white;
+
+
+}
+#folder {
+	background-color: transparent;
+	border: none;
+}
+
+a {
+	color: black;
+	text-decoration: none;
+}
+
 a:hover {
-	color: white;
+	color: black;
+	text-decoration: none;
 }
-
-#plate:hover {
-
-	opacity: 0.9;
-	dispay: block;
-
-}
-	.fa-file-o {
-
-
-
-		color: #3399ff;
-	}
-
-#proxy {
-			display: inline-block;
-			margin-left: 4em;
-	}
-	.fa-shekel {
-
-
-		color: #3399ff;
-		font-size: 80px;
-	}
-	B {
-		color: #3399ff;
-	}
-	.fa-folder-open {
-		color: #3399ff;
-	}
-	#form {
+#fileip {
+	
+	margin-left: 32em;
 	font-size: 15px;
-	color: #3399ff;
-}
 
+
+
+}
 
 </style>
-<div class="col-sm-9 box scrollbar" id="style-1">
-<h1 align="center"><i class="fa fa-shekel"></i><br/><BR/><b>FILE MANAGER</B></h1>
-<br/>
-<br/><div id="form"><form action="index.php" method="POST" class="form-group"><input type="text" name="dir" placeholder="c:// or any directory"><input type="submit" value="open the direcotry"></form></div>';
-echo '<b><i class="fa fa-folder-open"></i>&nbsp;&nbsp;', $dir, '</b><br/><br/>';
-
-
-
-$file = new file($dir);
 
 
 
 
-echo "</div>";
-echo '<div class="col-sm-3 text-center scrollbar sd" id="style-1">
+"
+;
 
-<h1 align="center"><i class="fa fa-shekel"></i></h1>
-<h1 align="center"><b>instructions</b></h1>
-<br/>
-<br/>
-<li>use delete option to delete files,the files wont be recovered after deleting</li>
-
-<br/>
-<br/>
-<li>cick to download the file,easy to download files.click on the file, you see.you will get downloaded.</li>
-
-
-
-<br/>
-<br/>
-<li>click to view the file in your browser.the file will be opened in the window</li>
+echo '<script type="text/javascript">
+	$(document).ready(function () {
+		var z = 700;
+		$(".color1").fadeIn(z, function () {
+			$(".color2").fadeIn(z);
+		});
+		
+	});
+</script>
+';
 
 
-<br/>
-<br/>
-<li>more features coming soon......</li>
+if (!empty($_FILES['file'])) {
 
+$name = $_FILES['file']['name'];
+$tmp_name = $_FILES['file']['tmp_name'];
+$location = $_SESSION['loc'];
 
-
-
-
-
-
-
-
-
-
-
-
-</div>';
-
+$upload = new file;
+$upload->upload($name, $tmp_name, $location);
 
 }
 
-?>
 
 
-
-
-
-
-
-
-
-
-
-
-<!--delete fie-->
-
-<?php 
-##################delete files
-if (!empty($_POST['file'])) {
-
-$dest = $_POST['file'];
-
-$delete = new deletefile($dest);
 
 
 
@@ -387,76 +636,113 @@ $delete = new deletefile($dest);
 
 
 
+
+
+
+
+
+
+
+
+
+##########################################################################################################################3########################################################################################################3
+
+
+
+
 ?>
 
 
 
+<script>
+
+
+
+	
+$(document).ready(
+			function king () {
+		
+var z = 800;
+
+$('.co').fadeIn(z).animate({Right: '250px'}, function () {
+	$('.addbar').fadeIn(z).animate({Left: '250px'}, function() {
+		$('.l1').fadeIn(z, function() {
+			$('.l2').fadeIn(z, function() {
+				$('.l3').fadeIn(z, function () {
+$('.l4').fadeIn(z, function() {
+
+	$('.l5').fadeIn(z, function() {
+		$('.l6').fadeIn(z);
 
 
 
 
 
-<!--password verification-->
+	});
+});
+
+
+
+				});
+			});
+
+		});
+	});
+});
+
+
+}
+
+
+
+
+
+);
+
+
+
+
+
+
+
+</script>
+
 
 <?php 
-//verify user name and password
+
 if (!empty($_POST['username']) && !empty($_POST['password'])) {
 
+if ($_POST['username'] == $username && $_POST['password'] == $password) {
 
 
-
-
-//change username and password according to your wish ############################33
-                           $username = "admin";                            #########################
-                           $password = "admin";                             ########################
-############################################################################
-
-if ($username == htmlentities($_POST['username']) && $password == htmlentities($_POST['password'])) {
-
-$_SESSION['name'] = str_shuffle("dfewkeoeritioeuabcdefgeforeirpoeiritrepotifeorjfeiojrfoejfoiefikwfkw;kweqjdpowkfakdapodkopfkofjwpfowpefopwekfoewpjfwihfwopwoipwirwpoeipwoekwpoewpoirrpwkfdpowkepfkeoifierfierweporopwe");
-
+$_SESSION['uq'] = "kwefheikhfiehfieuhtgirhgiuhriguhdighiuhgueihguiehgieuhgeiuhfgieuhfuiehtfiuehgiuehdguihdiughiuthgtiuhtur843t9854t894t9854yt8g7589yt756754t89g5498tg89tg489eyt4e9ytu498tu489eut894eut5894eut94uet98u4t89u4e9t8u54et8u48tu98tu4ut98u4ut9t54ue9tu9ut9t84";
 header("location: index.php");
 
+}
+
+
+
+
+
 
 }
 
 
 
 
-}
-else {
 
 
-}
+
+######################################################################################################
+
+
+
+
+
+
+
 
 
 ?>
 
-<style>
-	 *  STYLE 1
- */
 
-#style-1::-webkit-scrollbar-track
-{
-	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-	border-radius: 10px;
-	background-color: #F5F5F5;
-}
-
-#style-1::-webkit-scrollbar
-{
-	width: 12px;
-	background-color: #F5F5F5;
-}
-
-#style-1::-webkit-scrollbar-thumb
-{
-	border-radius: 10px;
-	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-	background-color: #555;
-}
-
-
-
-
-</style>
