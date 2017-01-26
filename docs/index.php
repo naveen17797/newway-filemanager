a1a143ebc7b59ad69495f9335e2ad6df541a13f6
naveen <kmnaveen101@gmail.com>
2017-01-26 09:24:22 +0530

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,50 @@
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="../css/font.css">
+<link rel="stylesheet" type="text/css" href="../css/materialize.css">
+<title>Newway Documentation</title>
+<style type="text/css">
+	@font-face {
+		font-family: ubuntu;
+		src: url("../fonts/ubuntu.ttf");
+	}
+	#head {
+		font-family: ubuntu;
+		height: 60px;
+		background-color: rgba(0,0,0,0.9);
+		color: white;
+		font-size: 40px;
+		text-transform: capitalize;
+	}
+	#content {
+		font-family: ubuntu;
+	line-height: 2em;
+	}
+	#heading {
+		font-size: 30px;
+	}
+	#paragraph {
+		font-size: 20px;
+	}
+</style>
+<div class="col-xs-12 col-lg-12 col-md-4 text-left navbar navbar-fixed-top" id="head">
+
+<b>N</b>ew way
+
+
+</div>
+
+<div class="col-xs-12 col-lg-12 col-md-4 text-left" id="content">
+<br/><br/><br/><br/><br/>
+
+<b id="heading"><i class='fa fa-folder' style="color: orange;"></i>&nbsp;New way File Manager</b>
+<br/><br/><p id="paragraph"><li> New way file manager is a web file manager for servers, it can perform Delete, Upload, Rename operations in server, it is improved to a greater extent in this update.</li><li>New way file manager has been provided with File Manager API to access files across the servers.</li>
+<li>New way file manager is written in PHP a web development language</li>
+<li>you can find the code for new way file manager in <a href="http://github.com/naveen17797/newway-filemanager">github</a></li>
+</p>
+<br/><br/>
+<b id="heading"><i class="fa fa-gear"></i>&nbsp;Operations</b>
+<br/><br/>
+<button class="btn btn-primary"><i class="fa fa-pencil"></i></button>&nbsp;&nbsp;<b id="heading">Rename</b>
+
+
+</div>
