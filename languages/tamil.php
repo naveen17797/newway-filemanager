b261683b5df99214e433c4931910c4993e94e903
naveen <kmnaveen101@gmail.com>
2017-09-06 13:55:31 +0530

    contains languages
    -this folder has languages which can be used in newway via its language manager

---
+++ languages/tamil.php
@@ -0,0 +1,90 @@
+<?php
+/**
+                            *@package: newway
+*
+                       *@author: Newway developer community
+*
+                         *@category: file manager
+*
+                      *@link http://github.com/naveen17797/newway
+*
+                   *#THIS FILE IS AN INTEGRAL COMPONENT OF NEWWAY V.1.0.0.0 VIBRANIUM. IT CAN BE MODIFIED, ALTERED, AND/OR *EDITED AS PER YOUR NEEDS. IT'S FREEWARE AND OPEN-SOURCE SOFTWARE.
+*
+*
+*
+
+*/
+
+
+// view page contents
+
+$newway['view_page_title'] = "காண்க | புதிய வழி";
+
+$newway['title'] = "புதிய வழி";
+
+$newway['browse_button'] = "உலாவ";
+
+$newway['upload_files'] = "கோப்பு பதிவேற்ற";
+
+$newway['create_folder'] = "கோப்புறையை உருவாக்கு";
+
+$newway['ubuntu_home'] = "Ubuntu home";
+
+$newway['server_home'] = "Server home";
+
+$newway['search'] = "தேடு";
+
+$newway['search_placeholder'] = "கோப்புறையை உலவ";
+
+$newway['log_out'] = "வெளியேறு";
+
+
+// create folder contents
+
+$newway['create_folder_title'] = "ஒரு கோப்புறையை உருவாக்குக | புதிய வழி";
+
+$newway['enter_folder_name'] = "நீங்கள் உருவாக்க விரும்பும் கோப்புறையின் பெயரை உள்ளிடவும்";
+
+$newway['failure'] = "தோல்வி";
+
+$newway['folder_failure_message'] = "நீங்கள் தட்டச்சு செய்துள்ள கோப்புறை ஏற்கனவே உள்ளது";
+
+//rename contents
+
+$newway['rename_title'] = "மறுபெயரிடு | புதிய வழி";
+
+$newway['rename_button_text'] = "மறுபெயரிடு";
+
+$newway['to'] = " ";
+
+$newway['rename'] = "கீழே உள்ள கோப்பினை மறுபெயரிடு";
+
+$newway['rename_placeholder'] = "புதிய பெயர்";
+
+$newway['rename_text'] = "மறுபெயரிடு";
+
+
+//upload file page contents
+
+$newway['upload_title'] = "பதிவேற்று | புதிய வழி";
+
+$newway['upload_files'] = "கோப்புகளை பதிவேற்றவும்";
+
+$newway['upload_button_text'] = "பதிவேற்ற";
+
+$newway['destination'] = "இலக்கு";
+//confirm_delete page contents
+
+$newway['confirm_delete_title'] = "இந்த கோப்பை / கோப்புறையை நீக்கவா?";
+
+$newway['yes'] = "ஆம், இந்த கோப்பு / கோப்புறையை நீக்கவும்";
+
+$newway['no'] = "இல்லை, இந்த கோப்பை / கோப்புறையை நீக்க வேண்டாம்";
+
+$newway['confirm_dialog_text'] = "இந்த கோப்பை / கோப்புறையை நிச்சயமாக நீக்க வேண்டுமா?";
+
+$newway['language_manager'] = "மொழி நிர்வாகி";
+
+$newway['newway_logs'] = "புதிய வழி பதிவுகள்";
+
+?>
