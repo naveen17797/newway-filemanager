dac6abe897c97e15c660e95b8e89e6faf7a8d694
José Borges <mail@joseborges.pt>
2017-09-29 10:31:51 +0100

    Update portuguese.php

--- languages/portuguese.php
+++ languages/portuguese.php
@@ -1,1 +1,93 @@
+<?php
+/**
+                            *@package: newway
+*
+                       *@author: Newway developer community
+*
+                         *@category: file manager
+*
+                      *@link http://github.com/naveen17797/newway
+*
+                   *#THIS FILE IS AN INTEGRAL COMPONENT OF NEWWAY V.1.0.0.0 VIBRANIUM. IT CAN BE MODIFIED, ALTERED, AND/OR *EDITED AS PER YOUR NEEDS. IT'S FREEWARE AND OPEN-SOURCE SOFTWARE.
+*
+*
+*
 
+*/
+
+
+// view page contents
+
+$newway['view_page_title'] = "Ver | Newway";
+
+$newway['title'] = "Newway";
+
+$newway['browse_button'] = "Navegar";
+
+$newway['upload_files'] = "Upload Ficheiros";
+
+$newway['create_folder'] = "Criar pasta";
+
+$newway['ubuntu_home'] = "Utilizador Ubuntu";
+
+$newway['server_home'] = "Raiz do Servidor";
+
+$newway['search'] = "pesquisar";
+
+$newway['search_placeholder'] = "Navegar Directoria";
+
+$newway['log_out'] = "Desconnectar";
+
+
+// create folder contents
+
+$newway['create_folder_title'] = "Criar Directoria | Newway";
+
+$newway['enter_folder_name'] = "Digite o nome da pasta que quer criar";
+
+$newway['failure'] = "falha";
+
+$newway['folder_failure_message'] = "A pasta que digitou já existe";
+
+$newway['destination'] = "destino";
+
+//rename contents
+
+$newway['rename_title'] = "Renomear | Newway";
+
+$newway['rename_button_text'] = "renomear";
+
+$newway['to'] = "para";
+
+$newway['rename'] = "renomear";
+
+
+$newway['rename_placeholder'] = "novo nome";
+$newway['rename_text'] = "renomear";
+
+//upload file page contents
+
+$newway['upload_title'] = "Upload | Newway";
+
+$newway['upload_filer'] = "UPLOAD FICHEIROS";
+
+$newway['upload_button_text'] = "upload ficheiros";
+
+
+//confirm_delete page contents
+
+$newway['confirm_delete_title'] = "confirma apagar este ficheiro/pasta?";
+
+$newway['yes'] = "sim, apagar este ficheiros/pasta";
+
+$newway['no'] = "no, voltar atrás";
+
+$newway['confirm_dialog_text'] = "quer realmente apagar este ficheiro/pasta?";
+
+$newway['language_manager'] = "gestão de idioma";
+
+
+//logs
+$newway['newway_logs'] = "Newway Logs";
+
+?>
