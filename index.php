947ec9afe267ac1da266c5549adf6322545c595d
naveen <kmnaveen101@gmail.com>
2019-09-19 20:18:04 +0530

    creating vue components

--- index.php
+++ index.php
@@ -2,12 +2,29 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"><i class="fa fa-shield"></i> &nbsp;Newway File Manager</a>
-  </div>
-</nav>
+<div id="navbar_content">
+	<title>{{ application_title }}</title>
 
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light sticky-top">
+	  <div class="container">
+	    <a class="navbar-brand" href="#"><i class="fa fa-shield"></i> &nbsp;{{ application_title }}</a>
+	  </div>
+	</nav>
+</div>
+
+<div class="col-sm-12 bg-dark color-light" style="height: 100%;">
+
+</div>
 
 <script type="text/javascript" src="js/vue.js"></script>
 <script type="text/javascript" src="js/vue-resource.js"></script>
+
+<script>
+	new Vue({
+		el: "#navbar_content",
+
+		data: {
+			application_title: "Newway File Manager"
+		}
+	})
+</script>
