8d7e48c1c188d40400a12df5b6711c467083b0a0
naveen <kmnaveen101@gmail.com>
2019-09-19 20:05:49 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#"><i class="fa fa-shield"></i> &nbsp;Newway File Manager</a>
+  </div>
+</nav>
+
+
+<script type="text/javascript" src="js/vue.js"></script>
+<script type="text/javascript" src="js/vue-resource.js"></script>
