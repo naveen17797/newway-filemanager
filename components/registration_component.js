63be9caf923df5cff9fe4c333a0ffff00fcebc3f
naveen <kmnaveen101@gmail.com>
2019-09-20 07:04:47 +0530

    adding registration component

---
+++ components/registration_component.js
@@ -0,0 +1,12 @@
+Vue.component('registration-component', {
+
+	template: "#registration_component_template",
+
+	data: function () {
+		return  {
+			email: "",
+			password:"",
+			access:1
+		}
+	},
+})
