57501d9169c84b927e6a54f6c281f4abcfbaec91
naveen <kmnaveen101@gmail.com>
2019-09-25 18:06:00 +0530

    file search added

--- components/file_folder_component.js
+++ components/file_folder_component.js
@@ -5,12 +5,24 @@ Vue.component('file-folder-component', {
 	props:{
 		files_and_folders_prop: null,
 		is_list_view: true,
+		search_string: "",
 	},
+
+	data: {
+		files_and_folders_prop_data: this.files_and_folders_prop
+	},
+
+
 	
 	methods: {
 
 		filterFilesAndReturnResult() {
+			console.log(this.files_and_folders_prop_data)
 			return this.files_and_folders_prop;
+		},
+
+		nameSearchChanged() {
+
 		}
 
 	}
