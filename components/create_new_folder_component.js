5a25bd6a790b56d3c396b6eb76fb11a6946ade9f
naveen <kmnaveen101@gmail.com>
2019-10-07 06:00:30 +0530

    adding create new folder component

---
+++ components/create_new_folder_component.js
@@ -0,0 +1,36 @@
+Vue.component('create-new-folder-component', {
+
+	template: "#create_new_folder_component_template",
+
+	props: {
+		api_url: {
+			type:String,
+			default: ""
+		},
+		
+	},
+
+
+	created() {
+
+		event_bus.$on('show-create-new-folder-modal', ()=> {
+			this.$refs.create_new_folder_modal.modal('show')
+			//$('#create_new_folder_modal').modal('show')
+		})
+
+	},
+
+	methods: {
+
+
+	},
+
+	data: function () {
+		return  {
+
+			folder_name:"",
+
+		}
+	}
+
+})
